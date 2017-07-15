c20e99611cfbb88b58ddbab30f9dc0d039c816cd
jims <jims@Sunday>
2017-07-15 18:09:49 +0800

    add wx sdk

--- src/WxTestBundle/Controller/DefaultController.php
+++ src/WxTestBundle/Controller/DefaultController.php
@@ -11,25 +11,38 @@ class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $token = 'suoklya';
+        $app = $this->get('wx_app');
 
+        // 从项目实例中得到服务端应用实例。
+        $server = $app->server;
+        $server->setMessageHandler(function ($message) {
+            // $message->FromUserName // 用户的 openid
+            // $message->MsgType // 消息类型：event, text....
+            return "您好！欢迎关注我!";
+        });
+        $response = $server->serve();
+
+        return $response;
+
+
+
+
+        /*$token = 'suoklya';
         $signature = $request->query->get('signature');
         $timestamp = $request->query->get('timestamp');
         $nonce = $request->query->get('nonce');
-        $echostr = $request->query->get('echostr');
+        $echostr = $request->query->get('echostr', '');
 
         $tmpArr = array($token, $timestamp, $nonce);
         sort($tmpArr, SORT_STRING);
         $tmpStr = implode( $tmpArr );
         $tmpStr = sha1( $tmpStr );
 
-        if( $tmpStr == $signature ){
-
+        if( $tmpStr == $signature && $echostr ){
             return new Response($echostr);
             //header('content-type:text');
             //echo $echostr;
         }
-
-        throw new NotFoundHttpException();
+        throw new NotFoundHttpException();*/
     }
 }
