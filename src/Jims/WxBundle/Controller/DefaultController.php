5fdc18ee4f2985bb095f25c747b02caf7be63b75
jims <jims@Sunday>
2017-07-22 23:17:08 +0800

    wx - 18

--- src/Jims/WxBundle/Controller/DefaultController.php
+++ src/Jims/WxBundle/Controller/DefaultController.php
@@ -11,12 +11,53 @@ class DefaultController extends Controller
     {
         file_put_contents('/tmp/xxxxx.log',$request->getUri());
 
-        $server         = $this->get('wx_app')->server;
-        $messageHandler = $this->get('wx.message.handler');
-        $server->setMessageHandler(function($message) use ($messageHandler) {
-            $messageHandler->handle($message);
+        //$server         = $this->get('wx_app')->server;
+        //$messageHandler = $this->get('wx.message.handler');
+        //$server->setMessageHandler(function($message) use ($messageHandler) {
+        //    $messageHandler->handle($message);
+        //});
+        //$response = $server->serve();
+        //return $response;
+
+
+        $app = $this->get('wx_app');
+
+        // 从项目实例中得到服务端应用实例。
+        $server = $app->server;
+        $server->setMessageHandler(function ($message) {
+            // $message->FromUserName // 用户的 openid
+            // $message->MsgType // 消息类型：event, text....
+            switch ($message->MsgType) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+            return "您好！欢迎关注我!";
         });
         $response = $server->serve();
+
         return $response;
     }
 
